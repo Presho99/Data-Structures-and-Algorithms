4da246958f00f9f80afaaabd6c9f0b079492fe7e
Precious <Preshomondi@gmail.com>
2025-10-04 21:04:40 +0300

    Linear search

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int list[100];
-    int n;
-    cout<<"";
+    int a[]={10,20,30,40,50};
+    int n=5;
+    int i,pos,value;
 
+    cout<<"Array elements are: \n";
+    for(i=0;i<n;i++){
+        cout<<"\nElement at index "<<i << "is" <<a[i];
+    }
+    cout<<"\nEnter position of nsertion of new element: ";
+    cin>>pos;
+    cout<<"\nEnter the new value or element to insert: ";
+
+    cin>>value;
+
+    for(i=n-1;i>pos;i--){
+        a[i+1]=a[i];
+    }
+
+return 0;
 }
---
+++ selectionsort/utube.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void 
+
+int main(){
+    int a[]={6,5,3,2,1,4};
+    int length=6;
+    return 0;
+    
+}
